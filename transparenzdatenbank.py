d6e7ffdef85f3894f6c91451d2bb22b53ae843f2
Michael Bauer <mihi@lo-res.org>
2012-12-08 09:36:34 +0100

    Saving to CSV and records done...

--- transparenzdatenbank.py
+++ transparenzdatenbank.py
@@ -2,12 +2,14 @@
 import requests
 from pyquery import PyQuery
 import re
+from pygeocoder import Geocoder,GeocoderError
 
 class Listing(object):
   
   def __init__(self,year=2011,url="http://www.transparenzdatenbank.at/trans/"):
     self.url=url
     self.year=year
+    self.hiddenfields=None
 
   def get_hiddenfields(self):
     params={"bundesland":0,"betrag":"0,0","betrag_jahr":self.year,"numberOfLinesPerPage":"100","name":""}
@@ -21,6 +23,8 @@ class Listing(object):
       return None
   
   def get_listing(self,number):
+    if not self.hiddenfields:
+      self.get_hiddenfields()
     params={"hiddenfields":self.hiddenfields, "move":u'\xA0%d\xA0'%number}
     r=requests.post("%sshow.detail"%self.url,data=params)
     pq=PyQuery(r.text)
@@ -34,8 +38,17 @@ class Listing(object):
   def get_all_listings(self):
     return reduce(lambda x,y: x+y, 
       map(lambda x: self.get_listing(x),range(1,self.max+1)))
+
+  def save_csv(self,filename):
+    pages=get_all_listings()
+    f=open(filename,"w")
+    f.write("%s/n"%",".join(pages[0].order))
+    for page in pages:
+      f.write(page.csv())
+    f.close()  
     
 class Page(object):
+  order=["unique","ukey","name","ort","lat","lon","program","amount","year"]
   
   def __init__(self,url="http://www.transparenzdatenbank.at/trans/",params=None):
     self.url="%sshow.detail"%url
@@ -61,4 +74,40 @@ class Page(object):
 
     items=[extract_items(row) for row in rows]
     return items
+ 
+  def get_geocode(self):
+    location=self.params["location"].encode("utf-8")
+    try:
+      geo=Geocoder.geocode(location)
+    except GeocoderError:
+      try:
+        geo=Geocoder.geocode("%s, Austria"%location.split(",")[1])
+      except GeocoderError:
+        return None
+    
+    return geo.coordinates
+    
 
+  def get_records(self):
+    (name,ort)=self.params["location"].encode("utf-8").split(",")
+    name.strip()
+    ort.lstrip()
+    geo=self.get_geocode()
+    if geo:
+      (lat,lon)=self.get_geocode()
+    else:
+      (lat,lon)=(None,None)
+    year=int(re.sub("[^0-9]","",self.params["year"]))
+    unique=self.params["unique"]
+
+    def construct_record(item):
+      return dict(item.items()+[("name",name.decode("utf-8")),("ort",ort.decode("utf-8")),("lat",lat),
+      ("lon",lon),("year",year),("unique",unique),("ukey",u"%s-%s"%(unique,item["program"]))])
+
+    return [construct_record(i) for i in self.get_items()] 
+  
+  def csv(self):
+    def line(r):
+      return u",".join([u"%s"%r[i] for i in self.order])
+
+    return u"%s\n"%"\n".join([line(r) for r in self.get_records()])  
